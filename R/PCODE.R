8deb3825e5661d947fd43adfd7fdffd970944c98
Unknown <haixuw@sfu.ca>
2018-11-15 11:25:31 -0800

    update on 1D numeric var

--- R/PCODE.R
+++ R/PCODE.R
@@ -1194,22 +1194,50 @@ numericvar <- function(data, time, ode.model,par.names,state.names, likelihood.f
 
         d_H2_theta2 <- (upper.eval - 2 * center.eval + lower.eval)/(stepsize^2)
 
+
+       #-------------------------------
+       H_deriv_wrt_y <- rep(NA, length(time))
+       inner_coef_upper <- nls_optimize.inner(innerobj, basis.initial = nuis.res,
+                                              ode.par = struc.res + stepsize,
+                                              derive.model = ode.model, input = inner.input,
+                                              options=list(maxeval = 50))$par
+       inner_coef_lower <- nls_optimize.inner(innerobj, basis.initial = nuis.res,
+                                              ode.par = struc.res - stepsize,
+                                              derive.model = ode.model, input = inner.input,
+                                              options=list(maxeval = 50))$par
+       obs_at_upper <- inner.input[[2]] %*%  inner_coef_upper
+       obs_at_lower <- inner.input[[2]] %*%  inner_coef_lower
+       for (index in 1:length(time)){
+         H_deriv_wrt_y[index] <- (data[index] + y_stepsize - obs_at_upper[index])^2 -
+                               (data[index] - y_stepsize - obs_at_upper[index])^2 -
+                               (data[index] + y_stepsize - obs_at_lower[index])^2 +
+                               (data[index] - y_stepsize - obs_at_lower[index])^2
+       }
+
+       if (length(par.names) == 1){
+          theta_deriv_wrt_y <- -(1/d_H2_theta2) * H_deriv_wrt_y
+       }else{
+
+       }
+
+      par.var <- theta_deriv_wrt_y %*% t(theta_deriv_wrt_y) * var(data)
+
+
+
+
+
+
+
       }else{
+
+
+
+
 
       }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+     return(par.var)
 }
