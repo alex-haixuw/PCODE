f2ca962954757813df19a49f5193990061dff1e6
Alekx <1nori11235@gmail.com>
2019-11-22 12:45:38 -0800

    License information
    Update license information

--- R/PCODE.R
+++ R/PCODE.R
@@ -1,3 +1,15 @@
+###########
+#
+#Copyright (C) <2019> <Haixu Wang>
+#
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>
+###########
+
+
 #' @title Parameter Cascade Method for Ordinary Differential Equation Models
 #' @description Obtain estimates of both structural and nuisance parameters of an ODE model by parameter cascade method.
 #' @usage pcode(data, time, ode.model, par.names, state.names,
